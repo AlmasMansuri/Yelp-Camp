b63651d7f2fa636103d71568f4ac1bcc93097908
Almas Mansuri <almas.a.mansuri@gmail.com>
2024-11-19 21:07:14 +0000

    changed port

--- app.js
+++ app.js
@@ -167,6 +167,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-app.listen(2000, () => {
-  console.log("listening on port 2000 http://localhost:2000 ");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
 });
